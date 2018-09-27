09b3bc95d407d7ab6872833564ed8c45cbf5908b
codeteo <ramones.teo@gmail.com>
2018-09-27 23:12:12 +0300

    Fetches and displays posts

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Home extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      this.setState({
+        posts: res.data.slice(0, 10)
+      });
+    });
+  }
   render() {
+    const { posts } = this.state;
+    const postsList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yet!</div>
+    );
     return (
       <div className="container">
         <h4 className="center">Home</h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-          eaque odit, obcaecati nesciunt voluptatem quos minima aut voluptates.
-          In corporis repellat qui id rem nisi repudiandae ipsam aperiam
-          recusandae inventore!
-        </p>
+        {postsList}
       </div>
     );
   }
