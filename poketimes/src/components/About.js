49bcace2527c58d9ee40d49c75a26a16d0a5c471
codeteo <ramones.teo@gmail.com>
2018-09-27 21:25:04 +0300

    Deletes logo, App.css and test files
    Adds Home component
    
    Adds About component
    
    Adds Contact component
    
    Adds Navbar component

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eaque
+        odit, obcaecati nesciunt voluptatem quos minima aut voluptates. In
+        corporis repellat qui id rem nisi repudiandae ipsam aperiam recusandae
+        inventore!
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eaque
+        odit, obcaecati nesciunt voluptatem quos minima aut voluptates. In
+        corporis repellat qui id rem nisi repudiandae ipsam aperiam recusandae
+        inventore!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
