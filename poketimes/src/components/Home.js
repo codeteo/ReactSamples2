32f7004661b7f1932b590ca96560758f7ccfc7c5
codeteo <ramones.teo@gmail.com>
2018-09-27 23:02:12 +0300

    Changes Home to class component

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eaque
-        odit, obcaecati nesciunt voluptatem quos minima aut voluptates. In
-        corporis repellat qui id rem nisi repudiandae ipsam aperiam recusandae
-        inventore!
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+          eaque odit, obcaecati nesciunt voluptatem quos minima aut voluptates.
+          In corporis repellat qui id rem nisi repudiandae ipsam aperiam
+          recusandae inventore!
+        </p>
+      </div>
+    );
+  }
+}
 
 export default Home;
