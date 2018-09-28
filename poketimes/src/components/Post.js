acde39e1163b2bd4899a012c3dd71839ce0ffd82
codeteo <ramones.teo@gmail.com>
2018-09-28 23:29:19 +0300

    Deletes axios from Post component

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,18 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 class Post extends Component {
-  state = {
-    post: null
-  };
-  componentDidMount() {
-    let id = this.props.match.params.post_id;
-    axios.get("https://jsonplaceholder.typicode.com/posts/" + id).then(res => {
-      this.setState({
-        post: res.data
-      });
-    });
-  }
   render() {
     const post = this.state.post ? (
       <div className="post">
