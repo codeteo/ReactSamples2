1199639882f7fbc3e31eafd1449180fbcf4d67aa
codeteo <ramones.teo@gmail.com>
2018-10-06 22:53:38 +0300

    Configs Firebase

---
+++ marioplan/src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAwIiY9KLshExj_vtGJkTJESBk1vG63Q40",
+  authDomain: "net-ninja-mariopan.firebaseapp.com",
+  databaseURL: "https://net-ninja-mariopan.firebaseio.com",
+  projectId: "net-ninja-mariopan",
+  storageBucket: "net-ninja-mariopan.appspot.com",
+  messagingSenderId: "171880948401"
+};
+
+firebase.initializeApp(config);
+firebase.firestore({ timestampsInSnapshots: true });
+
+export default firebase;
