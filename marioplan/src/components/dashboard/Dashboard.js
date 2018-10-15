ef433664c688ff007aa1abff26efbb24dd1ecc11
codeteo <ramones.teo@gmail.com>
2018-10-16 00:53:13 +0300

    Adds Route guarding

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -4,10 +4,12 @@ import ProjectList from "../projects/ProjectList";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
+import { Redirect } from "react-router-dom";
 
 class Dashboard extends Component {
   render() {
-    const { projects } = this.props;
+    const { projects, auth } = this.props;
+    if (!auth.uid) return <Redirect to="/signin" />;
 
     return (
       <div className="dashboard container">
@@ -28,9 +30,11 @@ const mapStateToProps = state => {
   console.log("MY_STATE: ", state);
 
   return {
-    projects: state.firestore.ordered.projects
+    projects: state.firestore.ordered.projects,
+    auth: state.firebase.auth
   };
 };
+
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([{ collection: "projects" }])
