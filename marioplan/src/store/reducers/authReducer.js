b52cc2fd3e00b761242952f16ea963076a8006d1
codeteo <ramones.teo@gmail.com>
2018-10-16 01:15:31 +0300

    feat: Implements SignUp

--- marioplan/src/store/reducers/authReducer.js
+++ marioplan/src/store/reducers/authReducer.js
@@ -15,9 +15,16 @@ const authReducer = (state = initState, action) => {
         authError: null
       };
     case "SIGNOUT_SUCCESS":
+      return state;
+    case "SIGNUP_SUCCESS":
       return {
         ...state,
         authError: null
+      };
+    case "SIGNUP_ERROR":
+      return {
+        ...state,
+        authError: action.err.message
       };
     default:
       return state;
